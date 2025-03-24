045aaee6636a811bd974bf7cef3fa73c10f6f43f
깔깔새우 <49589140+woduq1414@users.noreply.github.com>
2025-03-25 01:23:27 +0900

    feat : remove e2e test

--- tests/e2e/specs/page-options.test.ts
+++ tests/e2e/specs/page-options.test.ts
@@ -1,13 +1,13 @@
-import { canSwitchTheme } from '../helpers/theme.js';
+// import { canSwitchTheme } from '../helpers/theme.js';
 
-describe('Webextension Options Page', () => {
-  it('should make options page accessible', async () => {
-    const extensionPath = await browser.getExtensionPath();
-    const optionsUrl = `${extensionPath}/options/index.html`;
+// describe('Webextension Options Page', () => {
+//   it('should make options page accessible', async () => {
+//     const extensionPath = await browser.getExtensionPath();
+//     const optionsUrl = `${extensionPath}/options/index.html`;
 
-    await browser.url(optionsUrl);
+//     await browser.url(optionsUrl);
 
-    await expect(browser).toHaveTitle('Options');
-    await canSwitchTheme();
-  });
-});
+//     await expect(browser).toHaveTitle('Options');
+//     await canSwitchTheme();
+//   });
+// });
--- tests/e2e/specs/page-popup.test.ts
+++ tests/e2e/specs/page-popup.test.ts
@@ -1,12 +1,12 @@
-import { canSwitchTheme } from '../helpers/theme.js';
+// import { canSwitchTheme } from '../helpers/theme.js';
 
-describe('Webextension Popup', () => {
-  it('should open the popup successfully', async () => {
-    const extensionPath = await browser.getExtensionPath();
-    const popupUrl = `${extensionPath}/popup/index.html`;
-    await browser.url(popupUrl);
+// describe('Webextension Popup', () => {
+//   it('should open the popup successfully', async () => {
+//     const extensionPath = await browser.getExtensionPath();
+//     const popupUrl = `${extensionPath}/popup/index.html`;
+//     await browser.url(popupUrl);
 
-    await expect(browser).toHaveTitle('Popup');
-    await canSwitchTheme();
-  });
-});
+//     await expect(browser).toHaveTitle('Popup');
+//     await canSwitchTheme();
+//   });
+// });
